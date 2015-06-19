e42eff9e27383868434937ea89e9bf370094cee7
Satoshi Iwao <nirnirva.s.t.s1551@gmail.com>
2015-06-19 14:06:41 -0700

    this eclise and GitHub connection testing.

---
+++ TestProgram/src/testpackage/Main.java
@@ -0,0 +1,13 @@
+package testpackage;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String name = "satoshi";
+		
+		System.out.println("this is "+name);
+		System.out.println("hey. this is how to use eclipse and GitHub.");
+	}
+
+}
